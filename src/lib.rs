9a0b18765167b62368a8fad5023e0a5657da2494
David Irvine <david.irvine@maidsafe.net>
2015-04-09 13:22:28 +0100

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
     See the Licences for the specific language governing permissions and limitations relating to
     use of the MaidSafe Software.                                                                 */
 
-#![crate_name = "maidsafe_types"]
+#![crate_name = "lru_time_cache"]
 #![crate_type = "lib"]
 #![doc(html_logo_url = "http://maidsafe.net/img/Resources/branding/maidsafe_logo.fab2.png",
        html_favicon_url = "http://maidsafe.net/img/favicon.ico",
