1f915786ce32ad7520dd73a2681c348de916dab8
MaidSafe-QA <qa@maidsafe.net>
2016-08-03 07:46:22 +0000

    Updated documentation.

--- latest/implementors/core/clone/trait.Clone.js
+++ latest/implementors/core/clone/trait.Clone.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['lru_time_cache'] = ["impl&lt;Key, Value&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='lru_time_cache/struct.LruCache.html' title='lru_time_cache::LruCache'>LruCache</a>&lt;Key, Value&gt; <span class='where'>where Key: <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>, Value: <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a></span>",];
+implementors["lru_time_cache"] = ["impl&lt;Key, Value&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a> for <a class='struct' href='lru_time_cache/struct.LruCache.html' title='lru_time_cache::LruCache'>LruCache</a>&lt;Key, Value&gt; <span class='where'>where Key: <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a>, Value: <a class='trait' href='https://doc.rust-lang.org/nightly/core/clone/trait.Clone.html' title='core::clone::Clone'>Clone</a></span>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
